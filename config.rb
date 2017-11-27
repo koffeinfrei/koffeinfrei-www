034de0c06d1dc0df72caf7cd7555ab43516fe330
Alexis Reigel <mail@koffeinfrei.org>
2017-11-27 13:11:38 +0100

    add random quote

--- config.rb
+++ config.rb
@@ -1,14 +1,7 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
@@ -18,36 +11,18 @@ activate :blog do |blog|
 end
 
 activate :directory_indexes
-activate :asset_hash
+
+# the block config `activate :asset_hash do |opts|` doesn't work for
+# `asset_hash` as the values are set after the config block is yielded.
+activate :asset_hash, exts: config[:asset_extensions] - %w(.ico) + %w(.json)
 
 activate :livereload
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
 
 helpers do
   def inline_svg(name)
     File.read(File.join('source', 'images', "#{name}.svg"))
   end
 end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
 configure :build do
   activate :minify_css
